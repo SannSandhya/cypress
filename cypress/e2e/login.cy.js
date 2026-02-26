aaac24bfa19f3ba4bc62eddac6ff8de04cad1f15
SannSandhya <sandhyakhatiwada202@gmail.com>
2026-02-26 15:19:29 +0545

    added login tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,22 +4,41 @@ beforeEach(() => {
 })
 
 describe('Test for Login  ', () => {
-    it('TC-LOGIN-01:Test for valid username and valid password', () => {
+    it.skip('TC-LOGIN-01:Test for valid username and valid password', () => {
         cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
         cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
         cy.get('#login-button').should('be.visible').and('be.enabled').click()
         cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
         
     })
-    it('TC-LOGIN-02: Test for valid username and invalid password', () => {
+    it.skip('TC-LOGIN-02: Test for valid username and invalid password', () => {
         cy.get('#user-name').type('standard_user').should('have.value', 'standard_user')
-        cy.get('#password').type('')
+        cy.get('#password').type('sandhya')
         cy.get('login-button').should('be.visible').click()
         cy.get('#login-button').should('be.visible').and('be.enabled').click()
-        
+
     })
 
+    it('TC-LOGIN-03:Test for login with  username only',()=>{
+        cy.get('#user-name').type('standard_user').should('have.value','standard_user')
+        cy.get('#password').should('have.value', '')
+        cy.get('#login-button').should('be.visible').click()
+        cy.get('[data-test="error"]').should('have.text','Epic sadface: Password is required')
+
+    })
+    it('TC-LOGIN-04: Test for Login with password only',() =>{
+        cy.get('#user-name').should('have.value', '')
+        cy.get('#password').type('secret_sauce').should('have.value', 'secret_sauce')
+        cy.get('#login-button').should('be.visible').click()
+        cy.get('[data-test="error"]').should('have.text','Epic sadface: Username is required')
+        
+       
 
 
+
+
+
+
+    })
 
 })
